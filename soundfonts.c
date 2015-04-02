7fc05774c5ac31feed1ab0c90c7cb7c7c50d5b2c
Chris Penny <cpennny@gmail.com>
2015-04-02 00:15:00 -0400

    built something that made some sound in Pd

--- soundfonts.c
+++ soundfonts.c
@@ -7,11 +7,17 @@
 //
 
 #include "m_pd.h"
+#include "fluidsynth.h"
 
 static t_class *soundfonts_class;
 
 typedef struct _soundfonts {
     t_object x_obj;
+    
+    fluid_settings_t *settings;
+    fluid_synth_t *synth;
+    
+    t_int sfont_id;
     
     // Outlets
     t_outlet *x_outL;
@@ -24,6 +30,10 @@ void *soundfonts_new(void)
     // Get Class Instance
     t_soundfonts *instance = (t_soundfonts *)pd_new(soundfonts_class);
 
+    instance->settings = new_fluid_settings();
+    instance->synth = new_fluid_synth(instance->settings);
+    instance->sfont_id = fluid_synth_sfload(instance->synth, "/Users/ChrisPenny/Desktop/Comp150/Pd-for-LibPd/soundfonts/piano_1.sf2", 1);
+    
     // Spawn outlets
     instance->x_outL = outlet_new(&instance->x_obj, &s_signal);
     instance->x_outR = outlet_new(&instance->x_obj, &s_signal);
@@ -33,6 +43,8 @@ void *soundfonts_new(void)
 
 void soundfonts_free(t_soundfonts *instance)
 {
+    delete_fluid_settings(instance->settings);
+    delete_fluid_synth(instance->synth);
 
     outlet_free(instance->x_outL);
     outlet_free(instance->x_outR);
@@ -40,12 +52,13 @@ void soundfonts_free(t_soundfonts *instance)
 
 t_int *soundfonts_perform(t_int *input)
 {
+    t_soundfonts *instance = (t_soundfonts *)(input[1]);
     t_sample *outL = (t_sample *)(input[2]);
     t_sample *outR = (t_sample *)(input[3]);
+    int n = (int)(input[4]);
     
-    for(int i = 0; i < (int)(input[4]); i++) {
-        outL[i] = 14;
-        outR[i] = 28;
+    for(int i = 0; i < n; i++) {
+        fluid_synth_write_float(instance->synth, n, outL, 0, 1, outR, 0, 1);
     }
     
     return input + 5;
@@ -57,9 +70,14 @@ void soundfonts_dsp(t_soundfonts *instance, t_signal **sp)
             sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
 }
 
-void soundfonts_info(t_soundfonts *instance)
+void soundfonts_play(t_soundfonts *instance)
 {
-    post("soundfonts version 0.0");
+    fluid_synth_noteon(instance->synth, 0, 60, 127);
+}
+
+void soundfonts_stop(t_soundfonts *instance)
+{
+    fluid_synth_noteoff(instance->synth, 0, 60);
 }
 
 void soundfonts_setup(void)
@@ -72,6 +90,7 @@ void soundfonts_setup(void)
                                        0);
     
     class_addmethod(soundfonts_class, (t_method)soundfonts_dsp, gensym("dsp"), 0);
-    class_addmethod(soundfonts_class, (t_method)soundfonts_info, gensym("info"), 0);
+    class_addmethod(soundfonts_class, (t_method)soundfonts_play, gensym("play"), 0);
+    class_addmethod(soundfonts_class, (t_method)soundfonts_stop, gensym("stop"), 0);
 }
 
